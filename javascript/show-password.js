96b5cb6667b7c2ed2dc549e824378629ff70fbbc
Pamela Villegas <118470324+iampamelav@users.noreply.github.com>
2023-12-06 16:31:51 +0800

    Add js

---
+++ javascript/show-password.js
@@ -0,0 +1,12 @@
+let check = document.getElementById("check");
+let password = document.getElementById("password");
+
+check.onclick = function(){
+    if(password.type == "password"){
+        password.type = "text";
+        check.src = "../images/eye-open.png";
+    } else {
+        password.type = "password";
+        check.src = "../images/eye-close.png";
+    }
+}
