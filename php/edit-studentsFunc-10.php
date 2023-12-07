aec95058975399f50e5ccf74e693402e599a6776
Pamela Villegas <118470324+iampamelav@users.noreply.github.com>
2023-12-07 08:20:33 +0800

    Add files via upload

---
+++ php/edit-studentsFunc-10.php
@@ -0,0 +1,35 @@
+<?php
+    include('../php/connect.php');
+
+    $id = $_POST['ID'];
+    $name = $_POST['name'];
+    $lrn = $_POST['lrn'];
+    $email = $_POST['emailID'];
+    $mobilenum = $_POST['mobileNumber'];
+    $sex = $_POST['sex'];
+    $dob = $_POST['birthday'];
+    $address = $_POST['Address'];
+    $year = $_POST['year'];
+ 
+    if (isset($_POST['submit'])) {
+        
+        mysqli_query($conn,"UPDATE `grade-10` SET 
+        `Name`='$name', 
+        `LRN`='$lrn', 
+        `Email`='$email', 
+        `Mobile`='$mobilenum', 
+        `Sex`='$sex', 
+        `Birthday`='$dob', 
+        `Address`='$address', 
+        `Year`='$year' WHERE ID ='$id'");
+        
+        echo "<script> alert('Record successfully edited!')</script>";
+        echo "<script> window.open('../html/manage-stud-10.php', '_self')</script>";
+    }
+
+    else {
+        echo "<script> window.open('../html/manage-stud-10.php', '_self')</script>";
+    }
+    
+    
+?>
